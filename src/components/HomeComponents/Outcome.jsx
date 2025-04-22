6d9d6664f2ef0228dffe095dcbf53ff23a6e505c
gaurav-pndy <gauravpndy00@gmail.com>
2025-04-23 00:53:42 +0530

    fixed title

--- src/components/HomeComponents/Outcome.jsx
+++ src/components/HomeComponents/Outcome.jsx
@@ -29,7 +29,7 @@ const Outcome = () => {
     <section className="py-16 bg-gray-50">
       <div className="max-w-6xl mx-auto px-4">
         <h2 className="text-2xl  md:text-4xl font-bold text-center mb-10">
-          {t("prizes.title")}
+          {t("outcome.title")}
         </h2>
         <div className="grid  grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5 lg:gap-10 ">
           {courseItems.map((item, idx) => (
