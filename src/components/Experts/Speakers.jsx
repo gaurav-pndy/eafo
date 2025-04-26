07112ff2ca697fdbea1dbf7d3a7aaafe2e19ac9c
gaurav-pndy <gauravpndy00@gmail.com>
2025-04-26 23:52:54 +0530

    updated experts

--- src/components/Experts/Speakers.jsx
+++ src/components/Experts/Speakers.jsx
@@ -105,7 +105,7 @@ const Speakers = () => {
     <motion.div
       initial={{ opacity: 0, y: 30 }}
       whileInView={{ opacity: 1, y: 0 }}
-      viewport={{ amount: 0.3, once: true }}
+      viewport={{ once: true }}
       transition={{
         duration: 0.8,
         ease: "easeInOut",
